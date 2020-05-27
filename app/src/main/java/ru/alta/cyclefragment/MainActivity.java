dabf65078515d23739a2bca3cc220a568e389dc7
ganiushina <32090008+ganiushina@users.noreply.github.com>
2020-05-27 16:57:38 +0400

    Callback

--- app/src/main/java/ru/alta/cyclefragment/MainActivity.java
+++ app/src/main/java/ru/alta/cyclefragment/MainActivity.java
@@ -2,13 +2,39 @@ package ru.alta.cyclefragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SampleCallback {
+
+    String myMessage = "Stack Overflow is cool!";
+  //  Bundle bundle = new Bundle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void onClick(View v) {
+
+        BlankFragment fragment = new BlankFragment();
+        Bundle bundle = new Bundle();
+//        bundle.putInt(key, value);
+//        fragment.setArguments(bundle);
+//        Fragment frag1 = getFragmentManager().findFragmentById(R.id.fragment1);
+//        ((TextView) frag1.getView().findViewById(R.id.textView))
+//                .setText("Hello2");
+
+    }
+
+    @Override
+    public void onButtonClicked() {
+       TextView textView = findViewById(R.id.textView2);
+       textView.setText("Hello");
+    }
+
 }
---
+++ app/src/main/java/ru/alta/cyclefragment/SampleCallback.java
@@ -0,0 +1,5 @@
+package ru.alta.cyclefragment;
+
+public interface SampleCallback {
+    void onButtonClicked();
+}
